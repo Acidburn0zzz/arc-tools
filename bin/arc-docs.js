1db9713f567c89a3a7def68ac87356541b326503
Paweł Psztyć <jarrodek@gmail.com>
2016-09-23 19:37:06 +0100

    Update: Changed docs method, now it can work in a component directory

--- bin/arc-docs.js
+++ bin/arc-docs.js
@@ -11,16 +11,29 @@ const docs = require('../lib/arc-docs');
 program
   .usage('[options] [components...]')
   .option('-A, --all', 'Generate docs for all components')
-  .option('--verbose', 'Display messages')
-  .parse(process.argv);
+  .option('--verbose', 'Display messages');
+
+program.on('--help', () => {
+  console.log('  Don\'t use --all or list components if you want to generate doc in ' +
+    'current directory.');
+  console.log('');
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ arc docs --all # must be in components main directory');
+  console.log('    $ arc docs raml-js-parser file-drop # must be in components main directory');
+  console.log('    $ arc docs # must be in component\'s directory');
+  console.log('');
+});
+
+program.parse(process.argv);
 
 var pkgs = program.args;
 console.log();
-if (!pkgs.length && !program.all) {
-  console.log(colors.red('  No components specified. Use --all to process all components.'));
-  program.outputHelp();
-  process.exit(1);
-}
+// if (!pkgs.length && !program.all) {
+//   console.log(colors.red('  No components specified. Use --all to process all components.'));
+//   program.outputHelp();
+//   process.exit(1);
+// }
 var opts = {
   all: program.all || false,
   verbose: program.verbose || false
